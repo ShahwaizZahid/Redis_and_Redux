0d370d52259cc21dce040e7c1afba7b8ebdae8dd
ShahwaizZahid <shahwaizmughal940@gmail.com>
2026-04-27 10:47:10 +0500

    add: types of the typescript

--- client/src/types/Auth.ts
+++ client/src/types/Auth.ts
@@ -1,2 +1,9 @@
-export type SignupData = { name: string, email: string, password: string }
-export type verifyData = { email: string, otp: number }
+export type SignupData = { name: string; email: string; password: string };
+export type verifyData = { email: string; otp: number };
+export type LoginData = { email: string; password: string };
+
+export interface UserInfo {
+  _id: string;
+  email: string;
+  role: string;
+}
